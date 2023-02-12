d07aefba931232e40d92ff89ec999171cd16a199
Jonathan Beckett <jonbeckett30@gmail.com>
2023-02-12 15:06:34 -0600

    Initial commit
    Add the simple source file to be testing, the TFM submodule, and the
    demo ceedling project to demonstrate the inproper handling of inline
    functions when Cmock generates mocks.

---
+++ demo/test/test_func.c
@@ -0,0 +1,10 @@
+#include "unity.h"
+
+#include "func.h"
+
+#include "mock_crypto.h"
+
+void test_func_1(void)
+{
+    TEST_ASSERT(1 == 1);
+}
---
+++ src/func.c
@@ -0,0 +1,9 @@
+#include "psa/crypto.h"
+
+void func(void)
+{
+    int x = 1;
+    int y = 2;
+
+    int z = x + y;
+}
